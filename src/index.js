adf0a1c37bf1d7dde79e4310d40362639bdd37b3
Yuri Canuto <yuri.canuto@smplaces.com.br>
2022-01-02 01:54:17 -0300

    Ajustes de conflitos

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ function getBalance(statement) {
 function verifyIfExistsAccountCPF(request, response, next) {
     const { cpf } = request.headers
 
-    const customer = customers.find(customer => customer.cpf === cpf)
+    const customer = customers.find(customer => customer.cpf === cpf) 
 
     if (!customer) {
         return response.status(400).json({ error: 'Customer not found' })
